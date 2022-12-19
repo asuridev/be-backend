65ac56b4f218c06fe5ee71e90c72df4ed58acadf
antonio suarez <antoniosuarezrincon@gmail.com>
2022-12-19 06:28:28 -0500

    leyendo db

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 const express = require('express');
 const {conectDB} =  require('./db');
-
+const User = require('./models/users');
 
 conectDB();
 const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.get('/api/tickets', (req,res)=>{
-  res.json([
-    {
-      name:"Antonio",
-      lastname:"suarez"
-    },
-    {
-      name:"Marcos",
-      lastname:"Rincon"
-    }
-  ])
+app.get('/api/tickets', async (req,res)=>{
+  const users = await  User.find();
+  res.json(users);
 })
 
 
