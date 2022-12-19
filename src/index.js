405b6f36460c317e2efbcb685ff2d1c228d1881c
antonio suarez <antoniosuarezrincon@gmail.com>
2022-12-18 22:19:09 -0500

    ruta get

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.get('/', (req,res)=>{
+app.get('/api/tickets', (req,res)=>{
   res.send('Helo World')
 })
+
+
+app.use(express.static('public'));
 
 app.listen(port,()=>{
   console.log("escuchando el puerto...");
